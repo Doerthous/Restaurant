0d22a395c0205e225ca551a009a161a9709be893
YMS <yms@xxx.xxx>
2017-11-18 19:05:11 +0800

    ui test

--- src/programmer/test/ui/UITest.java
+++ src/programmer/test/ui/UITest.java
@@ -1,35 +1,54 @@
 package programmer.test.ui;
 
-import restaurant.service.vo.Employee;
+/*
+    ui层只依赖于service层：即只导入restaurant.service下的包
+ */
 import restaurant.service.IEmployeeService;
 import restaurant.service.ServiceFactory;
+import restaurant.service.vo.Employee;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.List;
 
 /**
  * Created by YMS on 2017/11/18.
+ *
+ * 如果ui层需要service层的服务，请在对应service层接口中注释该服务
+ *
+ * 如：
+ *      Employee的ui需要根据名字查询员工信息：
+ *          List<Employee> getEmployeeByName(String name);
+ *          将该声明注释在restaurant.service.IEmployeeService中，等待service层人员实现
  */
 public class UITest {
+    /*
+        这些是ui的组件
+     */
     private JPanel panel1;
     private JTextArea textArea1;
     private JButton button1;
+    /*
+        这是ui层与service层的接口
+     */
+    IEmployeeService service;
 
+    // 构造函数，可用于初始化一些成员变量
     public UITest() {
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                IEmployeeService service = ServiceFactory.getEmployeeService();
-                List<Employee> ems = service.getAllEmployee();
-                String text = "";
-                for(Employee em : ems){
-                    text += em.toString()+'\n';
-                }
-                textArea1.setText(text);
+        /*
+            初始化ui组件
+         */
+        button1.addActionListener(e -> {
+            List<Employee> ems = service.getAllEmployee();
+            String text = "";
+            for(Employee em : ems){
+                text += em.toString()+'\n';
             }
+            textArea1.setText(text);
         });
+        /*
+            初始化service层接口
+         */
+        service = ServiceFactory.getEmployeeService();
     }
 
     public static void main(String[] args) {
