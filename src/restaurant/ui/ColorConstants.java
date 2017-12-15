82413f81d10b6d1263bd3e71146383657d99c11f
Doerthous <827484343@qq.com>
2017-12-15 19:41:50 +0800

    project structrue refactoring

--- src/restaurant/ui/ColorConstants.java
+++
@@ -1,9 +0,0 @@
-package restaurant.ui;
-
-import java.awt.*;
-
-public interface ColorConstants {
-    Color title = new Color(0xFF,0x99,0x00);
-    Color background = new Color(0xF6,0xC2,0x48);
-    Color subtitle = new Color(0xB8, 0x65, 0x3B);
-}
